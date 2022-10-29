92642dd3b5d385594a7ca8031beba09660b31cd5
Julian Viso <julian.viso.romero@gmail.com>
2022-10-29 14:07:21 -0300

    TG-39: initial commit, connect server

--- src/Components/Projects/index.js
+++ src/Components/Projects/index.js
@@ -1,9 +1,24 @@
+import { useEffect, useState } from 'react';
 import styles from './projects.module.css';
 
 function Projects() {
+  const [projects, saveProjects] = useState([]);
+
+  useEffect(() => {
+    fetch(`${process.env.REACT_APP_API_URL}projects`)
+      .then((response) => response.json())
+      .then((response) => {
+        saveProjects(response.data);
+      });
+  }, []);
   return (
     <section className={styles.container}>
-      <h2>Projects</h2>
+      {projects.map((project) => (
+        <div key={project._id}>
+          <h2>{project.name}</h2>
+          <p>{project.clientName}</p>
+        </div>
+      ))}
     </section>
   );
 }
