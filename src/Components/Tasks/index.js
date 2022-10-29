fd061565f6fdd0e822bcfca7a886d7c399c91f47
Juan Ignacio Vescovo <jivescovo@gmail.com>
2022-10-29 19:18:45 -0300

    TG-36: Add add-task button and its CSS

--- src/Components/Tasks/index.js
+++ src/Components/Tasks/index.js
@@ -1,9 +1,69 @@
+import { useEffect, useState } from 'react';
 import styles from './tasks.module.css';
+import Task from './Task/index';
+//import Form from './Form/index';
+import Modal from './Modal/index';
 
 function Tasks() {
+  const [tasks, saveTasks] = useState([]);
+  const [showModal, setShowModal] = useState(false);
+  const [taskId, setTaskId] = useState(undefined);
+
+  useEffect(async () => {
+    try {
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/tasks`);
+      const data = await response.json();
+      saveTasks(data.data);
+    } catch (error) {
+      console.error(error);
+    }
+  }, []);
+
+  const closeModal = () => {
+    setShowModal(false);
+  };
+
+  const deleteTask = async (id) => {
+    saveTasks([...tasks.filter((task) => task._id !== id)]);
+    await fetch(`${process.env.REACT_APP_API_URL}/tasks/${id}`, {
+      method: 'DELETE'
+    });
+  };
+
   return (
     <section className={styles.container}>
-      <h2>Tasks</h2>
+      <Modal
+        show={showModal}
+        closeModal={closeModal}
+        deleteTask={deleteTask}
+        taskId={taskId}
+        title="Do you want to delete this task?"
+      />
+      <div>
+        <div>
+          <h2>Tasks</h2>
+        </div>
+        <div>
+          <a href="/tasks/form">
+            <button className={styles.addTaskButton}>Add new task</button>
+          </a>
+        </div>
+      </div>
+      <table>
+        <thead>
+          <tr>
+            <th>Description</th>
+            <th>Delete Task</th>
+          </tr>
+        </thead>
+        <tbody>
+          {tasks.map((task) => {
+            return (
+              <Task key={task._id} task={task} setShowModal={setShowModal} setTaskId={setTaskId} />
+            );
+          })}
+        </tbody>
+      </table>
     </section>
   );
 }
