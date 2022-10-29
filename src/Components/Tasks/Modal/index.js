7e87510e3dffb58e10a962631334aee6dd81990c
Juan Ignacio Vescovo <jivescovo@gmail.com>
2022-10-29 10:45:45 -0300

    TG-35: Edit modal to match figma specs

--- src/Components/Tasks/Modal/index.js
+++ src/Components/Tasks/Modal/index.js
@@ -12,16 +12,23 @@ function Modal(props) {
   return (
     <div className={styles.container}>
       <div className={styles.modal}>
-        <h3>{props.title}</h3>
-        <div>
-          <button
-            onClick={() => {
-              onCloseModal();
-            }}
-          >
-            Confirm
-          </button>
-          <button onClick={props.closeModal}>Cancel</button>
+        <h3 className={styles.modalTitle}>{props.title}</h3>
+        <div className={styles.buttonsDiv}>
+          <div>
+            <button onClick={props.closeModal} className={styles.buttonCancel}>
+              Cancel
+            </button>
+          </div>
+          <div>
+            <button
+              className={styles.buttonConfirm}
+              onClick={() => {
+                onCloseModal();
+              }}
+            >
+              Confirm
+            </button>
+          </div>
         </div>
       </div>
     </div>
