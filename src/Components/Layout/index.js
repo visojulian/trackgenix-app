a66218f31165bdf170c8a0872b340da7b48d1edd
Julian Viso <julian.viso.romero@gmail.com>
2022-10-30 18:59:13 -0300

    TG-40: form project route added

--- src/Components/Layout/index.js
+++ src/Components/Layout/index.js
@@ -6,6 +6,7 @@ import Home from '../Home/index';
 import styles from './layout.module.css';
 import Employees from '../Employees/index';
 import Projects from '../Projects';
+import ProjectsForm from '../Projects/Form';
 import TimeSheets from '../TimeSheets';
 import Tasks from '../Tasks/index';
 
@@ -23,6 +24,9 @@ function Layout() {
       break;
     case '/projects':
       currentScreen = <Projects />;
+      break;
+    case '/projects/form':
+      currentScreen = <ProjectsForm />;
       break;
     case '/time-sheets':
       currentScreen = <TimeSheets />;
--- src/Components/Projects/index.js
+++ src/Components/Projects/index.js
@@ -4,6 +4,9 @@ function Projects() {
   return (
     <section className={styles.container}>
       <h2>Projects</h2>
+      <a className={styles.button} href="/projects/form">
+        Add New Project
+      </a>
     </section>
   );
 }
