0d2f0e8ba1861a8aaeb82144370d8d89fa83c7e7
Julian Viso <julian.viso.romero@gmail.com>
2022-10-31 20:46:59 -0300

    TG-40: auto complete edit project form

--- src/Components/Projects/Form/index.js
+++ src/Components/Projects/Form/index.js
@@ -1,7 +1,38 @@
-import React from 'react';
+// import styles from './form.module.css';
+
+import { useState } from 'react';
 
 const ProjectForm = () => {
-  return <div>ProjectForm</div>;
+  const search = window.location.search;
+  const [project, setProject] = useState({});
+  const [nameValue, setNameValue] = useState('');
+  const onChangeNameInput = (e) => {
+    setNameValue(e.target.value);
+  };
+
+  if (search.match('id=')) {
+    const id = search.substring(search.indexOf('id=') + 3);
+
+    fetch(`${process.env.REACT_APP_API_URL}/projects/${id}`)
+      .then((response) => response.json())
+      .then((response) => {
+        setProject(response.data);
+        setNameValue(project.name);
+      });
+  }
+
+  return (
+    <form>
+      <input
+        id="name"
+        name="name"
+        placeholder="Project name"
+        required
+        value={nameValue}
+        onChange={onChangeNameInput}
+      />
+    </form>
+  );
 };
 
 export default ProjectForm;
--- src/Components/Projects/index.js
+++ src/Components/Projects/index.js
@@ -1,14 +1,74 @@
+import { useEffect, useState } from 'react';
+import DeleteConfirmationModal from './DeleteConfirmationModal';
+import Delete from './assets/trash.png';
 import styles from './projects.module.css';
 
-function Projects() {
+const Projects = () => {
+  const [projects, saveProjects] = useState([]);
+  const [selectedProject, setSelectedProject] = useState({});
+  const [showModal, saveShowModal] = useState(false);
+
+  useEffect(() => {
+    fetch(`${process.env.REACT_APP_API_URL}/projects`)
+      .then((response) => response.json())
+      .then((response) => {
+        saveProjects(response.data);
+      });
+  }, []);
+
+  const deleteProject = async (id) => {
+    await fetch(`${process.env.REACT_APP_API_URL}/projects/${id}`, {
+      method: 'DELETE'
+    });
+    saveProjects([...projects.filter((project) => project._id !== id)]);
+  };
+
+  const handleDelete = (project) => {
+    setSelectedProject({ id: project._id, name: project.name });
+    saveShowModal(true);
+  };
+
+  const editProject = (id) => {
+    window.location.assign(`/projects/form?id=${id}`);
+  };
+
   return (
     <section className={styles.container}>
-      <h2>Projects</h2>
+      <DeleteConfirmationModal
+        show={showModal}
+        handleModal={saveShowModal}
+        deleteProject={deleteProject}
+        project={selectedProject}
+      />
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <th>Project</th>
+            <th>Created</th>
+            <th></th>
+            <th>
+              <img src={Delete}></img>
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {projects.map((project) => (
+            <tr key={project._id} onClick={() => editProject(project._id)}>
+              <td>{project.name}</td>
+              <td>{project.startDate.slice(0, 10)}</td>
+              <td className={styles.center}>&hellip;</td>
+              <td className={styles.center}>
+                <button onClick={() => handleDelete(project)}>&times;</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
       <a className={styles.button} href="/projects/form">
         Add New Project
       </a>
     </section>
   );
-}
+};
 
 export default Projects;
