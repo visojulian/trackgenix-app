53a7b8166fd60ca0f9ce53c5f83d381db63a4828
Maximiliano Licarzze <maxilicarzze@gmail.com>
2022-10-31 17:42:44 -0300

    TG-37: index creation

--- src/Components/TimeSheets/TimeSheet/index.js
+++ src/Components/TimeSheets/TimeSheet/index.js
@@ -1,9 +1,27 @@
-const TimeSheet = ({ timeSheet }) => {
+//import Task from '../Task/index';
+import Trash from '../../../assets/trash.png';
+const TimeSheet = ({ timeSheet, setShowModal, setTimeSheetId }) => {
   return (
-    <tr>
-      <td>{timeSheet.description}</td>
+    <tr style={{ display: 'flex', justifyContent: 'center' }}>
+      <td style={{ flexBasis: '10%' }}>{timeSheet.description}</td>
+      <td style={{ flexBasis: '10%' }}>{timeSheet.date}</td>
+      <td style={{ flexBasis: '10%' }}>{timeSheet.hours}</td>
+      <td style={{ flexBasis: '10%' }}>{timeSheet.task.description}</td>
+      <td style={{ flexBasis: '10%' }}>
+        {`${timeSheet.employee.name} ${timeSheet.employee.lastName}`}
+      </td>
+      <td style={{ flexBasis: '10%' }}>{timeSheet.project.name}</td>
+      <td style={{ flexBasis: '10%' }}>
+        <img
+          src={Trash}
+          alt="trash"
+          onClick={() => {
+            setShowModal(true);
+            setTimeSheetId(timeSheet._id);
+          }}
+        />
+      </td>
     </tr>
   );
 };
-
 export default TimeSheet;
--- src/Components/TimeSheets/index.js
+++ src/Components/TimeSheets/index.js
@@ -1,10 +1,12 @@
 import { useEffect, useState } from 'react';
-import styles from './time-sheets.module.css';
+import styles from './list.module.css';
 import TimeSheet from './TimeSheet/index';
-//import Modal from './Modal/index';
+import Modal from './Modal/index';
 
 const TimeSheets = () => {
   const [timeSheets, setTimeSheet] = useState([]);
+  const [showModal, setShowModal] = useState(false);
+  const [timeSheetId, setTimeSheetId] = useState(undefined);
 
   useEffect(async () => {
     try {
@@ -16,19 +18,49 @@ const TimeSheets = () => {
     }
   }, []);
 
+  const closeModal = () => {
+    setShowModal(false);
+  };
+
+  const deleteTimeSheet = async (id) => {
+    setTimeSheet([...timeSheets.filter((timeSheet) => timeSheet._id !== id)]);
+    await fetch(`${process.env.REACT_APP_API_URL}/time-sheets/${id}`, {
+      method: 'DELETE'
+    });
+  };
+
   return (
     <section className={styles.container}>
       <h2>TimeSheets</h2>
+      <Modal
+        show={showModal}
+        closeModal={closeModal}
+        deleteTimeSheet={deleteTimeSheet}
+        timeSheetId={timeSheetId}
+        title="Are you sure that you want to delete this time sheet?"
+      />
       <table>
         <thead>
-          <tr>
-            <th>Description</th>
-            <th>Delete TimeSheet</th>
+          <tr style={{ display: 'flex', justifyContent: 'center' }}>
+            <th style={{ flexBasis: '10%' }}>Description</th>
+            <th style={{ flexBasis: '10%' }}>Date</th>
+            <th style={{ flexBasis: '10%' }}>Hours</th>
+            <th style={{ flexBasis: '10%' }}>Task</th>
+            <th style={{ flexBasis: '10%' }}>Employee</th>
+            <th style={{ flexBasis: '10%' }}>Project</th>
+            <th style={{ flexBasis: '10%' }}>Delete TimeSheet</th>
           </tr>
         </thead>
         <tbody>
           {timeSheets.map((timeSheet) => {
-            return <TimeSheet key={timeSheets._id} timeSheet={timeSheet} />;
+            return (
+              <TimeSheet
+                key={timeSheets._id}
+                timeSheet={timeSheet}
+                setTimeSheetId={setTimeSheetId}
+                setShowModal={setShowModal}
+              />
+            );
           })}
         </tbody>
       </table>
