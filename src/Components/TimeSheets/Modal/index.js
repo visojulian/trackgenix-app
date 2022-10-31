b67a5def95ad0d8b23c6dc54e8fbcd2a1795d317
Maximiliano Licarzze <maxilicarzze@gmail.com>
2022-10-31 19:13:47 -0300

    TG-37: components changes, css fixes

--- src/Components/TimeSheets/Modal/index.js
+++ src/Components/TimeSheets/Modal/index.js
@@ -14,21 +14,17 @@ const ConfirmationModal = (props) => {
       <div className={styles.box}>
         <h3 className={styles.title}>{props.title}</h3>
         <div className={styles.buttonsDiv}>
-          <div>
-            <button onClick={props.closeModal} className={styles.cancelButton}>
-              Cancel
-            </button>
-          </div>
-          <div>
-            <button
-              className={styles.confirmButton}
-              onClick={() => {
-                onCloseModal();
-              }}
-            >
-              Confirm
-            </button>
-          </div>
+          <button onClick={props.closeModal} className={styles.cancelButton}>
+            Cancel
+          </button>
+          <button
+            className={styles.confirmButton}
+            onClick={() => {
+              onCloseModal();
+            }}
+          >
+            Confirm
+          </button>
         </div>
       </div>
     </div>
