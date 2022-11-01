2d502ec28936873a12ba7fa4b6b13b27343d8caf
Camila Rojas <camilavaleriarojas1@gmail.com>
2022-11-01 12:34:15 -0300

    TG-32: add put functinality and apply css styles

--- src/Components/SuperAdmins/Form/index.js
+++ src/Components/SuperAdmins/Form/index.js
@@ -1,33 +1,98 @@
 import styles from './form.module.css';
-// import React, { useStates} from "react";
+import React, { useState } from 'react';
 
 const Form = () => {
+  const [addSuperAdmin, setAddSuperAdmin] = useState({
+    name: '',
+    lastName: '',
+    email: '',
+    password: ''
+  });
+
+  const onChange = (e) => {
+    setAddSuperAdmin({ ...addSuperAdmin, [e.target.name]: e.target.value });
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    const res = await fetch(`${process.env.REACT_APP_API_URL}/super-admins`, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        name: addSuperAdmin.name,
+        lastName: addSuperAdmin.lastName,
+        email: addSuperAdmin.email,
+        password: addSuperAdmin.password
+      })
+    });
+    setAddSuperAdmin({
+      name: '',
+      lastName: '',
+      email: '',
+      password: ''
+    });
+
+    const content = await res.json();
+    console.log(content);
+  };
+
   return (
     <section className={styles.container}>
       <div className={styles.flex}>
         <h3>Add Super Admin</h3>
-        <div className={styles.box}>
+        <form className={styles.box} onSubmit={onSubmit}>
           <div>
             <div className={styles.div}>
               <label>Name</label>
-              <input></input>
+              <input
+                type="text"
+                required
+                name="name"
+                value={addSuperAdmin.name}
+                onChange={onChange}
+              />
             </div>
             <div className={styles.div}>
               <label>Last Name</label>
-              <input></input>
+              <input
+                type="text"
+                required
+                name="lastName"
+                value={addSuperAdmin.lastName}
+                onChange={onChange}
+              />
             </div>
           </div>
           <div>
             <div className={styles.div}>
               <label>Email</label>
-              <input></input>
+              <input
+                required
+                type="text"
+                name="email"
+                value={addSuperAdmin.email}
+                onChange={onChange}
+              />
+            </div>
+            <div className={styles.div}>
+              <label>Password</label>
+              <input
+                required
+                type="text"
+                name="password"
+                value={addSuperAdmin.password}
+                onChange={onChange}
+              />
             </div>
             <div className={styles.buttons}>
-              <button>Cancel</button>
-              <button>Submit</button>
+              <button onClick={() => window.location.assign('/super-admins')}>Cancel</button>
+              <button type="submit">Submit</button>
             </div>
           </div>
-        </div>
+        </form>
       </div>
     </section>
   );
