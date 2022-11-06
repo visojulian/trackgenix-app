a88e8877c2953c097b5b3b09703d54cdaa844431
Julian Viso <julian.viso.romero@gmail.com>
2022-11-05 21:01:32 -0300

    TG-54: add fixes after PR feedback

--- src/Components/TimeSheets/TimeSheet/index.js
+++ src/Components/TimeSheets/TimeSheet/index.js
@@ -2,44 +2,41 @@ import Trash from '../../../assets/trash.png';
 import styles from './time.module.css';
 
 const TimeSheet = ({ timeSheet, setShowModal, setTimeSheetId }) => {
-  if (
-    timeSheet.description &&
-    timeSheet.date &&
-    timeSheet.hours &&
-    timeSheet.task &&
-    timeSheet.employee &&
-    timeSheet.project
-  ) {
-    return (
-      <tr
-        onClick={() => {
-          window.location.assign(`/time-sheets/form?id=${timeSheet._id}`);
-        }}
-        style={{ display: 'flex', justifyContent: 'center' }}
-      >
-        <td style={{ flexBasis: '25%' }}>{timeSheet.description}</td>
-        <td style={{ flexBasis: '15%' }}>{timeSheet.date}</td>
-        <td style={{ flexBasis: '10%' }}>{timeSheet.hours}</td>
-        <td style={{ flexBasis: '20%' }}>{timeSheet.task.description}</td>
-        <td style={{ flexBasis: '20%' }}>
-          {`${timeSheet.employee.name} ${timeSheet.employee.lastName}`}
-        </td>
-        <td style={{ flexBasis: '10%' }}>{timeSheet.project.name}</td>
-        <td style={{ flexBasis: '10%' }}>
-          <img
-            className={styles.button}
-            src={Trash}
-            alt="trash"
-            onClick={(event) => {
-              event.stopPropagation();
-              setShowModal(true);
-              setTimeSheetId(timeSheet._id);
-            }}
-          />
-        </td>
-      </tr>
-    );
-  } else return null;
+  return (
+    <tr
+      onClick={() => {
+        window.location.assign(`/time-sheets/form?id=${timeSheet._id}`);
+      }}
+      style={{ display: 'flex', justifyContent: 'center' }}
+    >
+      <td style={{ flexBasis: '25%' }}>{timeSheet.description}</td>
+      <td style={{ flexBasis: '15%' }}>{timeSheet.date}</td>
+      <td style={{ flexBasis: '10%' }}>{timeSheet.hours}</td>
+      <td style={{ flexBasis: '20%' }}>
+        {timeSheet.task ? timeSheet.task.description : 'Task does not exist'}
+      </td>
+      <td style={{ flexBasis: '20%' }}>
+        {timeSheet.employee
+          ? `${timeSheet.employee.name} ${timeSheet.employee.lastName}`
+          : 'Employee does not exist'}
+      </td>
+      <td style={{ flexBasis: '10%' }}>
+        {timeSheet.project ? timeSheet.project.name : 'Project does not exist'}
+      </td>
+      <td style={{ flexBasis: '10%' }}>
+        <img
+          className={styles.button}
+          src={Trash}
+          alt="trash"
+          onClick={(event) => {
+            event.stopPropagation();
+            setShowModal(true);
+            setTimeSheetId(timeSheet._id);
+          }}
+        />
+      </td>
+    </tr>
+  );
 };
 
 export default TimeSheet;
