e8825fe790c4879a3b0ab895a172d4b305467ade
Juan Ignacio Vescovo <jivescovo@gmail.com>
2022-11-12 13:32:13 -0300

    TG-72: Add reducer to handle tasks actions

---
+++ src/redux/task/reducer.js
@@ -0,0 +1,61 @@
+import {
+  GET_TASKS_PENDING,
+  GET_TASKS_SUCCESS,
+  GET_TASKS_ERROR,
+  DELETE_TASKS_PENDING,
+  DELETE_TASKS_SUCCESS,
+  DELETE_TASKS_ERROR
+} from './constants';
+
+const INITAL_STATE = {
+  list: [{ id: 'test', description: 'placeholder' }],
+  isLoading: false,
+  error: ''
+};
+
+const reducer = (state = INITAL_STATE, action) => {
+  switch (action.type) {
+    case GET_TASKS_PENDING:
+      return {
+        ...state,
+        isLoading: true
+      };
+    case GET_TASKS_SUCCESS:
+      return {
+        ...state,
+        isLoading: false,
+        error: '',
+        list: action.payload
+      };
+    case GET_TASKS_ERROR:
+      return {
+        ...state,
+        isLoading: false,
+        error: action.payload,
+        list: []
+      };
+    case DELETE_TASKS_PENDING:
+      return {
+        ...state,
+        isLoading: true
+      };
+    case DELETE_TASKS_SUCCESS:
+      console.log(state);
+      return {
+        ...state,
+        list: [...state.list.filter((task) => task._id !== action.payload)],
+        isLoading: false,
+        error: ''
+      };
+    case DELETE_TASKS_ERROR:
+      return {
+        ...state,
+        isLoading: false,
+        error: action.payload
+      };
+    default:
+      return state;
+  }
+};
+
+export default reducer;
