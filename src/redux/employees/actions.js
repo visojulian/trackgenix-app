81de315fe3c1bb0750368cec6fce86f94b7d7cee
Camila Rojas <camilavaleriarojas1@gmail.com>
2022-11-12 20:11:18 -0300

    TG-66: add actions and constants to delete employees

--- src/redux/employees/actions.js
+++ src/redux/employees/actions.js
@@ -1,4 +1,11 @@
-import { GET_EMPLOYEES_PENDING, GET_EMPLOYEES_SUCCESS, GET_EMPLOYEES_ERROR } from './constants';
+import {
+  GET_EMPLOYEES_PENDING,
+  GET_EMPLOYEES_SUCCESS,
+  GET_EMPLOYEES_ERROR,
+  DELETE_EMPLOYEES_PENDING,
+  DELETE_EMPLOYEES_SUCCESS,
+  DELETE_EMPLOYEES_ERROR
+} from './constants';
 
 export const getEmployeesPending = () => {
   return {
@@ -16,6 +23,26 @@ export const getEmployeesSuccess = (payload) => {
 export const getEmployeesError = (error) => {
   return {
     type: GET_EMPLOYEES_ERROR,
+    payload: error
+  };
+};
+
+export const deleteEmployeesPending = () => {
+  return {
+    type: DELETE_EMPLOYEES_PENDING
+  };
+};
+
+export const deleteEmployeesSuccess = (payload) => {
+  return {
+    type: DELETE_EMPLOYEES_SUCCESS,
+    payload
+  };
+};
+
+export const deleteEmployeesError = (error) => {
+  return {
+    type: DELETE_EMPLOYEES_ERROR,
     payload: error
   };
 };
