ec4c4c7f0f5b57632bc9dda36eede64996e44c1c
Camila Rojas <camilavaleriarojas1@gmail.com>
2022-11-14 17:58:31 -0300

    TG-67: add actions and constants to post and put an employee

--- src/redux/employees/actions.js
+++ src/redux/employees/actions.js
@@ -4,7 +4,13 @@ import {
   GET_EMPLOYEES_ERROR,
   DELETE_EMPLOYEE_PENDING,
   DELETE_EMPLOYEE_SUCCESS,
-  DELETE_EMPLOYEE_ERROR
+  DELETE_EMPLOYEE_ERROR,
+  POST_EMPLOYEE_PENDING,
+  POST_EMPLOYEE_SUCCESS,
+  POST_EMPLOYEE_ERROR,
+  PUT_EMPLOYEE_PENDING,
+  PUT_EMPLOYEE_SUCCESS,
+  PUT_EMPLOYEE_ERROR
 } from './constants';
 
 export const getEmployeesPending = () => {
@@ -43,6 +49,46 @@ export const deleteEmployeeSuccess = (payload) => {
 export const deleteEmployeeError = (error) => {
   return {
     type: DELETE_EMPLOYEE_ERROR,
+    payload: error
+  };
+};
+
+export const postEmployeePending = () => {
+  return {
+    type: POST_EMPLOYEE_PENDING
+  };
+};
+
+export const postEmployeeSuccess = (payload) => {
+  return {
+    type: POST_EMPLOYEE_SUCCESS,
+    payload
+  };
+};
+
+export const postEmployeeError = (error) => {
+  return {
+    type: POST_EMPLOYEE_ERROR,
+    payload: error
+  };
+};
+
+export const putEmployeePending = () => {
+  return {
+    type: PUT_EMPLOYEE_PENDING
+  };
+};
+
+export const putEmployeeSuccess = (payload) => {
+  return {
+    type: PUT_EMPLOYEE_SUCCESS,
+    payload
+  };
+};
+
+export const putEmployeeError = (error) => {
+  return {
+    type: PUT_EMPLOYEE_ERROR,
     payload: error
   };
 };
