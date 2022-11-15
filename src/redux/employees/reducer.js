7b335b02808ba39e6a8010a3b7d39f0f6d707a89
Camila Rojas <camilavaleriarojas1@gmail.com>
2022-11-14 22:37:27 -0300

    TG-67: add reducers to post and put an employee

--- src/redux/employees/reducer.js
+++ src/redux/employees/reducer.js
@@ -4,7 +4,13 @@ import {
   GET_EMPLOYEES_ERROR,
   DELETE_EMPLOYEE_PENDING,
   DELETE_EMPLOYEE_SUCCESS,
-  DELETE_EMPLOYEE_ERROR
+  DELETE_EMPLOYEE_ERROR,
+  POST_EMPLOYEE_PENDING,
+  POST_EMPLOYEE_SUCCESS,
+  POST_EMPLOYEE_ERROR,
+  PUT_EMPLOYEE_PENDING,
+  PUT_EMPLOYEE_SUCCESS,
+  PUT_EMPLOYEE_ERROR
 } from './constants';
 
 const INITIAL_STATE = {
@@ -23,16 +29,16 @@ const reducer = (state = INITIAL_STATE, action) => {
     case GET_EMPLOYEES_SUCCESS:
       return {
         ...state,
+        list: action.payload,
         isLoading: false,
-        error: '',
-        list: action.payload
+        error: ''
       };
     case GET_EMPLOYEES_ERROR:
       return {
         ...state,
+        list: [],
         isLoading: false,
-        error: action.payload,
-        list: []
+        error: action.payload
       };
     case DELETE_EMPLOYEE_PENDING:
       return {
@@ -42,11 +48,48 @@ const reducer = (state = INITIAL_STATE, action) => {
     case DELETE_EMPLOYEE_SUCCESS:
       return {
         ...state,
+        list: [...state.list.filter((employee) => employee._id !== action.payload)],
         isLoading: false,
-        error: '',
-        list: [...state.list.filter((employee) => employee._id !== action.payload)]
+        error: ''
       };
     case DELETE_EMPLOYEE_ERROR:
+      return {
+        ...state,
+        isLoading: false,
+        error: action.error
+      };
+    case POST_EMPLOYEE_PENDING:
+      return {
+        ...state,
+        isLoading: true
+      };
+    case POST_EMPLOYEE_SUCCESS:
+      return {
+        ...state,
+        list: [...state.list, action.payload],
+        isLoading: false,
+        error: ''
+      };
+    case POST_EMPLOYEE_ERROR:
+      return {
+        ...state,
+        list: [],
+        isLoading: false,
+        error: action.payload
+      };
+    case PUT_EMPLOYEE_PENDING:
+      return {
+        ...state,
+        isLoading: true
+      };
+    case PUT_EMPLOYEE_SUCCESS:
+      return {
+        ...state,
+        list: [...state.list, action.payload],
+        isLoading: false,
+        error: ''
+      };
+    case PUT_EMPLOYEE_ERROR:
       return {
         ...state,
         isLoading: false,
