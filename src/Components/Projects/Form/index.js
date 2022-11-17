61e4e330da020f642c94007daa0b4d601a3bf124
Julian Viso <julian.viso.romero@gmail.com>
2022-11-17 09:57:53 -0300

    TG-78: fix modal and project styles

--- src/Components/Projects/Form/index.js
+++ src/Components/Projects/Form/index.js
@@ -311,6 +311,7 @@ const ProjectForm = () => {
               </div>
             </div>
             <Table
+              className={styles.employeeList}
               data={newArr()}
               headers={['name', 'role', 'rate']}
               onDelete={handleDelete}
