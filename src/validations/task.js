ca619f0fbf5e794dd81d753e4a4b48128760b5cb
licamus <maxilicarzze@gmail.com>
2022-11-19 20:26:21 -0300

    TG-88: Joi validations

--- src/validations/task.js
+++ src/validations/task.js
@@ -1,1 +1,9 @@
-// import Joi from "joi";
+import Joi from 'joi';
+
+export const schema = Joi.object({
+  name: Joi.string().max(150).required().messages({
+    'string.empty': 'Description cannot be empty',
+    'string.max': 'Description cannot exceed 20 characters',
+    'strig.required': 'Description is required'
+  })
+});
