31fdad8046c40f67a00d222cbe55341841714f9b
Julian Viso <julian.viso.romero@gmail.com>
2022-11-21 11:17:35 -0300

    TG-94: list worked hours

---
+++ src/Components/Employee/ListWorkedHours/index.js
@@ -0,0 +1,77 @@
+import React, { useEffect, useState } from 'react';
+import { useHistory, useParams } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { getTimesheets } from 'redux/timeSheets/thunks';
+import { getProjects } from 'redux/projects/thunks';
+import { Button, Modal, Spinner, Table } from 'Components/Shared';
+import styles from './list.module.css';
+import { getTasks } from 'redux/task/thunks';
+
+const ListWorkedHours = () => {
+  const history = useHistory();
+  const { id } = useParams();
+  const dispatch = useDispatch();
+  const [showModal, setShowModal] = useState(false);
+  const {
+    list: timesheets,
+    isLoading: timesheetsIsLoading,
+    error: timesheetsError
+  } = useSelector((state) => state.timeSheets);
+
+  useEffect(() => {
+    dispatch(getTimesheets());
+    dispatch(getProjects());
+    dispatch(getTasks());
+  }, []);
+
+  const employeeTimesheets = timesheets.filter((timesheet) => timesheet.employee._id === id);
+
+  const tableData = employeeTimesheets.map((timesheet) => {
+    return {
+      description: timesheet.description,
+      hours: timesheet.hours,
+      task: timesheet.task.description,
+      project: timesheet.project.name
+    };
+  });
+
+  if (timesheetsIsLoading) {
+    return <Spinner isLoading={true} />;
+  }
+
+  if (timesheetsError) {
+    console.log('error');
+    return (
+      <Modal
+        isOpen={showModal}
+        handleClose={() => {
+          setShowModal();
+        }}
+        isActionModal={false}
+        action={() => history.goBack()}
+      >
+        There was an error
+      </Modal>
+    );
+  }
+
+  return (
+    <div className={styles.container}>
+      <Table
+        data={tableData}
+        headers={['Timsheet', 'Task', 'Project', 'Hours']}
+        values={['description', 'task', 'project', 'hours']}
+      />
+      <Button
+        text="Cancel"
+        type="button"
+        variant="secondary"
+        onClick={() => {
+          history.goBack();
+        }}
+      />
+    </div>
+  );
+};
+
+export default ListWorkedHours;
--- src/routes/employees.js
+++ src/routes/employees.js
@@ -2,6 +2,7 @@ import React, { lazy } from 'react';
 import { Route, Switch } from 'react-router-dom/cjs/react-router-dom';
 const Employees = lazy(() => import('Components/Employees'));
 const EmployeesForm = lazy(() => import('Components/Employees/Form'));
+const ListWorkedHours = lazy(() => import('Components/Employee/ListWorkedHours'));
 
 const employees = () => {
   return (
@@ -9,6 +10,7 @@ const employees = () => {
       <Route exact path="/employees" component={Employees} />
       <Route exact path="/employees/form" component={EmployeesForm} />
       <Route path="/employees/form/:id" component={EmployeesForm} />
+      <Route path="/employees/list-hours/:id" component={ListWorkedHours} />
     </Switch>
   );
 };
