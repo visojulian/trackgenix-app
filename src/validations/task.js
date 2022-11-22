2aebb29a5e8b3214330a83a27ce15506857ea171
Maximiliano Licarzze <maxilicarzze@gmail.com>
2022-11-21 23:39:51 -0300

    TG-88: trim validation for task

--- src/validations/task.js
+++ src/validations/task.js
@@ -1,7 +1,7 @@
 import Joi from 'joi';
 
 export const schema = Joi.object({
-  description: Joi.string().max(150).required().messages({
+  description: Joi.string().trim().max(150).required().messages({
     'string.empty': 'Description is required',
     'string.max': 'Description cannot exceed 150 characters'
   })
