423bb5e13f33c2a7bedeb8878e39a2ffcacb9327
Juan Ignacio Vescovo <jivescovo@gmail.com>
2022-11-22 18:53:59 -0300

    TG-84: Remove comments from shared component

--- src/Components/Shared/TextInput/index.js
+++ src/Components/Shared/TextInput/index.js
@@ -8,12 +8,9 @@ const TextInput = ({ label, id, name, type, placeholder, register, error }) => {
         id={id}
         name={name}
         {...register(name)}
-        //value={value}
-        //onChange={onChange}
         className={styles.input}
         type={type}
         placeholder={placeholder}
-        //required
       />
       {error && <p className={styles.error}>{error}</p>}
     </div>
