afb817bf4c315f0c33cc00972222c76a4d1cbef8
Maximiliano Licarzze <maxilicarzze@gmail.com>
2022-11-22 22:33:10 -0300

    TG-87: merge routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,7 @@ import { Switch, Route, BrowserRouter, Redirect } from 'react-router-dom';
 const AdminsRoutes = lazy(() => import('./admins'));
 const SuperAdminsRouter = lazy(() => import('./superAdmins'));
 const EmployeeRouter = lazy(() => import('./employees'));
+const TimeSheetsRouter = lazy(() => import('./timeSheets'));
 const HomeRouter = lazy(() => import('./home'));
 
 const Routes = () => {
@@ -14,6 +15,7 @@ const Routes = () => {
           <Route path="/admins" component={AdminsRoutes} />
           <Route path="/super-admins" component={SuperAdminsRouter} />
           <Route path="/employees" component={EmployeeRouter} />
+          <Route path="/time-sheets" component={TimeSheetsRouter} />
           <Route path="/home" component={HomeRouter}></Route>
           <Redirect to="/home"></Redirect>
         </Switch>
