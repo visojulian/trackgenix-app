ea0bca1db837d6c1129eb4a4b7e65491d14f5106
Juan Ignacio Vescovo <jivescovo@gmail.com>
2022-11-26 17:16:06 -0300

    TG-113: Create redux thunks

---
+++ src/redux/auth/thunks.js
@@ -0,0 +1,43 @@
+import {
+  loginPending,
+  //setLoggedIn,
+  loginError,
+  logoutPending,
+  //setLoggedOut,
+  logoutError
+} from './actions';
+
+import { signInWithEmailAndPassword, signOut } from 'firebase/auth';
+import { auth } from '../../helpers/firebase';
+
+export const login = (data) => {
+  return async (dispatch) => {
+    dispatch(loginPending());
+    try {
+      const userCredentials = await signInWithEmailAndPassword(auth, data.email, data.password);
+
+      const {
+        token,
+        claims: { role }
+      } = await userCredentials.user.getIdTokenResult();
+
+      sessionStorage.setItem(('token', token));
+      return role;
+    } catch (error) {
+      return dispatch(loginError());
+    }
+  };
+};
+
+export const logout = () => {
+  return (dispatch) => {
+    dispatch(logoutPending());
+    return signOut(auth)
+      .then(() => {
+        sessionStorage.clear();
+      })
+      .catch((error) => {
+        return dispatch(logoutError(error.toString()));
+      });
+  };
+};
