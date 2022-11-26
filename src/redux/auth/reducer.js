e743437d3d2f6f30b76ff97539e658b41e6cf5ce
Juan Ignacio Vescovo <jivescovo@gmail.com>
2022-11-26 17:15:34 -0300

    TG-113: Create redux reducer

---
+++ src/redux/auth/reducer.js
@@ -0,0 +1,57 @@
+import {
+  LOGIN_PENDING,
+  LOGIN_SUCCESS,
+  LOGIN_ERROR,
+  LOGOUT_PENDING,
+  LOGOUT_SUCCESS,
+  LOGOUT_ERROR
+  //SIGNUP_PENDING,
+  //SIGNUP_SUCCESS,
+  //SIGNUP_ERROR
+} from './constants';
+
+const INITIAL_STATE = {
+  email: null,
+  //password: null,
+  role: null,
+  isLoading: false,
+  error: null
+};
+
+const reducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case LOGIN_PENDING:
+    case LOGOUT_PENDING:
+      return {
+        ...state,
+        isLoading: true
+      };
+    case LOGIN_ERROR:
+    case LOGOUT_ERROR:
+      return {
+        ...state,
+        isLoading: false,
+        error: action.payload
+      };
+    case LOGIN_SUCCESS:
+      return {
+        ...state,
+        isLoading: false,
+        role: action.payload.role,
+        email: action.payload.email,
+        error: null
+      };
+    case LOGOUT_SUCCESS:
+      return {
+        ...state,
+        isLoading: false,
+        role: null,
+        email: null,
+        error: null
+      };
+    default:
+      return state;
+  }
+};
+
+export default reducer;
