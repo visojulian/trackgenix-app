dd4915fbd9b9b46cb7ae47dae8198f168d748800
Maxi Delfino <maxidelfino1234@hotmail.com>
2022-11-28 16:47:20 -0300

    TG-112: Route division by role

--- src/Components/Auth/Login/index.js
+++ src/Components/Auth/Login/index.js
@@ -22,7 +22,7 @@ const Login = () => {
     mode: 'onBlur'
   });
 
-  const onSubmit = async (data) => {
+  const onSubmit = (data) => {
     if (Object.values(errors).length === 0) {
       dispatch(login(data)).then((data) => {
         if (data.type === LOGIN_SUCCESS) {
