acd59448be993a8bed0bee11e9f697843ec312a3
Maxi Delfino <maxidelfino1234@hotmail.com>
2022-11-28 11:21:07 -0300

    TG-112: Route division by role

--- src/routes/superAdmins.js
+++ src/routes/superAdmins.js
@@ -3,8 +3,14 @@ import React, { lazy } from 'react';
 import { useRouteMatch, Redirect, Route, Switch } from 'react-router-dom';
 const SuperAdmins = lazy(() => import('Components/SuperAdmins'));
 const SuperAdminsForm = lazy(() => import('Components/SuperAdmins/Form'));
+const Admins = lazy(() => import('Components/Admins'));
+const AdminForm = lazy(() => import('Components/Admins/AdminForm'));
 
-const routes = [{ name: 'home', path: '/super-admins' }];
+const routes = [
+  { name: 'Home', path: '/super-admins' },
+  { name: 'Super Admins', path: '/super-admins' },
+  { name: 'Admins', path: '/super-admins/admins' }
+];
 const SuperAdminsRouter = () => {
   const { url } = useRouteMatch();
   return (
@@ -13,6 +19,9 @@ const SuperAdminsRouter = () => {
         <Route exact path={`${url}/`} component={SuperAdmins} />
         <Route exact path={`${url}/form`} component={SuperAdminsForm} />
         <Route path={`${url}/form/:id`} component={SuperAdminsForm} />
+        <Route exact path={`${url}/admins`} component={Admins} />
+        <Route exact path={`${url}/admins/form`} component={AdminForm} />
+        <Route path={`${url}/admins/form/:id`} component={AdminForm} />
         <Redirect to={`${url}/`} />
       </Switch>
     </Layout>
