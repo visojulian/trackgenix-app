8a31d1355d18085a26ca6c46dc1bb3103c612d80
Maxi Delfino <maxidelfino1234@hotmail.com>
2022-11-28 11:51:13 -0300

    TG-112: Solving deploy velcer

--- src/routes/timeSheets.js
+++ src/routes/timeSheets.js
@@ -1,6 +1,7 @@
 import Layout from 'Components/Layout';
 import React, { lazy } from 'react';
-import { useRouteMatch, Route, Switch } from 'react-router-dom';
+import { useRouteMatch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom/cjs/react-router-dom';
 const TimeSheets = lazy(() => import('Components/TimeSheets'));
 const TimeSheetsForm = lazy(() => import('Components/TimeSheets/Form'));
 
