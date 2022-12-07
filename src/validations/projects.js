c2c56614235a07fc0e7874352c8ed5a536fabb6b
Maximiliano Licarzze <maxilicarzze@gmail.com>
2022-12-07 18:44:29 -0300

    TG-132: Creation of new trim validations

--- src/validations/projects.js
+++ src/validations/projects.js
@@ -1,13 +1,13 @@
 import Joi from 'joi';
 
 export const schema = Joi.object({
-  name: Joi.string().required().min(3).max(50).messages({
+  name: Joi.string().trim().required().min(3).max(50).messages({
     'string.required': 'Name is required',
     'string.empty': 'Name cannot be empty',
     'string.min': 'Name must have at least 3 characters',
     'string.max': 'Name cannot exceed 50 characters'
   }),
-  description: Joi.string().required().min(3).max(150).messages({
+  description: Joi.string().trim().required().min(3).max(150).messages({
     'string.required': 'Description is required',
     'string.empty': 'Description cannot be empty',
     'string.min': 'Name must have at least 3 characters',
@@ -20,13 +20,13 @@ export const schema = Joi.object({
     'date.base': 'End date is required',
     'date.greater': 'End cannot be greater than Start Date'
   }),
-  clientName: Joi.string().required().min(3).max(50).messages({
+  clientName: Joi.string().trim().required().min(3).max(50).messages({
     'string.required': 'Client Name is required',
     'string.empty': 'Client Name cannot be empty',
     'string.min': 'Client Name must have at least 3 characters',
     'string.max': 'Client Name cannot exceed 50 characters'
   }),
-  employee: Joi.string().length(24).required().messages({
+  employee: Joi.string().trim().length(24).required().messages({
     'string.required': 'Employee is required',
     'string.empty': 'Employee cannot be empty',
     'string.length': 'The ID of this employee is not valid.'
@@ -37,6 +37,7 @@ export const schema = Joi.object({
   }),
   rate: Joi.string()
     .required()
+    .trim()
     .pattern(/^[0-9]+$/)
     .messages({
       'string.required': ' Rate is required',
