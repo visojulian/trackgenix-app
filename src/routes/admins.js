0ab71e37ce3558a70ed0e71ee83b90b74524bfea
Camila Rojas <camilavaleriarojas1@gmail.com>
2022-12-08 11:33:26 -0300

    TG-128: edit admin routes and add admin profile to render info

--- src/routes/admins.js
+++ src/routes/admins.js
@@ -1,29 +1,26 @@
 import Layout from 'Components/Layout';
 import React, { lazy } from 'react';
 import { useRouteMatch, Redirect, Route, Switch } from 'react-router-dom';
-const Admins = lazy(() => import('Components/Admins'));
+const AdminProfile = lazy(() => import('Components/Admins/AdminProfile'));
 const Employees = lazy(() => import('Components/Employees'));
 const EmployeeForm = lazy(() => import('Components/Employees/Form'));
 const Projects = lazy(() => import('Components/Projects/index'));
 const ProjectsForm = lazy(() => import('Components/Projects/Form/index'));
 const Tasks = lazy(() => import('Components/Tasks/index'));
 const TasksForm = lazy(() => import('Components/Tasks/Form/index'));
-const TimeSheets = lazy(() => import('Components/TimeSheets'));
-const TimeSheetsForm = lazy(() => import('Components/TimeSheets/Form'));
 
 const routes = [
   { name: 'Home', path: '/admins' },
   { name: 'Employees', path: '/admins/employees' },
   { name: 'Projects', path: '/admins/projects' },
-  { name: 'Tasks', path: '/admins/tasks' },
-  { name: 'Timesheets', path: '/admins/time-sheets' }
+  { name: 'Tasks', path: '/admins/tasks' }
 ];
 const AdminsRouter = () => {
   const { url } = useRouteMatch();
   return (
     <Layout routes={routes}>
       <Switch>
-        <Route exact path={`${url}/`} component={Admins} />
+        <Route exact path={`${url}/`} component={AdminProfile} />
         <Route exact path={`${url}/employees`} component={Employees} />
         <Route path={`${url}/employees/form/:id`} component={EmployeeForm} />
         <Route exact path={`${url}/admins/projects`} component={Projects} />
@@ -32,9 +29,6 @@ const AdminsRouter = () => {
         <Route exact path={`${url}/admins/tasks`} component={Tasks} />
         <Route exact path={`${url}/admins/tasks/form`} component={TasksForm} />
         <Route path={`${url}/admins/tasks/form/:id`} component={TasksForm} />
-        <Route exact path={`${url}/admins/time-sheets`} component={TimeSheets} />
-        <Route exact path={`${url}/admins/time-sheets/form`} component={TimeSheetsForm} />
-        <Route path={`${url}/admins/time-sheets/form/:id`} component={TimeSheetsForm} />
         <Redirect to={`${url}/`} />
       </Switch>
     </Layout>
