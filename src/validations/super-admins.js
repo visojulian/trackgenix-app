8847a1c6ac5c3f91cc22d1431dc3fe46fe8148dc
Maxi Delfino <maxidelfino1234@hotmail.com>
2022-12-10 17:49:49 -0300

    TG-136: You can edit user

--- src/validations/super-admins.js
+++ src/validations/super-admins.js
@@ -53,3 +53,48 @@ export const schema = Joi.object({
     'any.only': 'Passwords must match'
   })
 });
+
+export const editSchema = Joi.object({
+  name: Joi.string()
+    .min(3)
+    .max(20)
+    .alphanum()
+    .trim()
+    .pattern(/^([^0-9]*)$/i)
+    .required()
+    .messages({
+      'string.empty': 'Name cannot be empty',
+      'string.min': 'Name must have at least 3 characters',
+      'string.max': 'Name cannot exceed 20 characters',
+      'string.alphanum': 'Name cannot not have special characters',
+      'string.pattern.base': 'Name can only have letters',
+      'string.required': 'Name is required'
+    }),
+  lastName: Joi.string()
+    .min(3)
+    .max(25)
+    .alphanum()
+    .trim()
+    .pattern(/^([^0-9]*)$/i)
+    .required()
+    .messages({
+      'string.empty': 'Last Name cannot be empty',
+      'string.min': 'Last Name must have at least 3 characters',
+      'string.max': 'Last Name cannot exceed 25 characters',
+      'string.alphanum': 'Last Name cannot not have special characters',
+      'string.pattern.base': 'Last Name can only have letters',
+      'string.required': 'Last Name is required'
+    }),
+  email: Joi.string()
+    .email({ tlds: { allow: false } })
+    .trim()
+    .required()
+    .messages({
+      'string.empty': 'Email cannot be empty',
+      'string.email': 'Email needs to be a valid email address',
+      'string.required': 'Email is required'
+    }),
+  repeatEmail: Joi.any().valid(Joi.ref('email')).required().messages({
+    'any.only': 'Emails must match'
+  })
+});
