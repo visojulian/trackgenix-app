54d8edf8da541a61e47ad21f8e17970b33b368c5
Julieta Preliasco Benitez <preliascojulieta@gmail.com>
2022-12-10 19:34:45 -0300

    TG-128: implement timesheets logic to admin role

--- src/routes/admins.js
+++ src/routes/admins.js
@@ -8,12 +8,14 @@ const Projects = lazy(() => import('Components/Projects/index'));
 const ProjectsForm = lazy(() => import('Components/Projects/Form/index'));
 const Tasks = lazy(() => import('Components/Tasks/index'));
 const TasksForm = lazy(() => import('Components/Tasks/Form/index'));
+const TimeSheets = lazy(() => import('Components/Admins/TimeSheets'));
 
 const routes = [
   { name: 'Home', path: '/admin' },
   { name: 'Employees', path: '/admin/employees' },
   { name: 'Projects', path: '/admin/projects' },
-  { name: 'Tasks', path: '/admin/tasks' }
+  { name: 'Tasks', path: '/admin/tasks' },
+  { name: 'Timesheets', path: '/admin/timesheets' }
 ];
 const AdminsRouter = () => {
   const { url } = useRouteMatch();
@@ -30,6 +32,7 @@ const AdminsRouter = () => {
         <Route exact path={`${url}/tasks`} component={Tasks} />
         <Route exact path={`${url}/tasks/form`} component={TasksForm} />
         <Route path={`${url}/tasks/form/:id`} component={TasksForm} />
+        <Route exact path={`${url}/timesheets`} component={TimeSheets} />
         <Redirect to={`${url}/`} />
       </Switch>
     </Layout>
