a001c98769391333e63d7b5389da58e32c6ccabe
Camila Rojas <camilavaleriarojas1@gmail.com>
2022-12-16 00:45:43 -0300

    TG-145: add main section

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -1,11 +1,53 @@
 import styles from './home.module.css';
 
-function Home() {
+const Home = () => {
   return (
-    <section className={styles.container}>
-      <h2>Home</h2>
-    </section>
+    <div className={styles.container}>
+      <section>
+        <div className={styles.appName}>
+          <h1>
+            Track<span>GENIX</span>
+          </h1>
+          <p>AN EASY SYSTEM TO TRACK YOUR TEAM´S TIMES AND PROJECTS</p>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum
+            has been the industrys standard dummy text ever since the 1500s.
+          </p>
+        </div>
+        <div className={styles.box}>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div className={styles.rocket}>
+          <div className={styles.rocketBody}>
+            <div className={styles.body}></div>
+            <div className={styles.finLeft}></div>
+            <div className={styles.finRight}></div>
+            <div className={styles.window}></div>
+          </div>
+          <div className={styles.exhaustFlame}></div>
+          <ul className={styles.exhaustFumes}>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+        </div>
+      </section>
+    </div>
   );
-}
+};
 
 export default Home;
