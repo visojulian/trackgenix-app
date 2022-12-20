8b630b4e6d2ee7399bc9083c5c0d0aa76658513d
Camila Rojas <camilavaleriarojas1@gmail.com>
2022-12-20 19:56:00 -0300

    TG-160: implement responsive design

--- src/Components/Admins/AdminProfile/index.js
+++ src/Components/Admins/AdminProfile/index.js
@@ -27,24 +27,26 @@ const AdminProfile = () => {
 
   return (
     <div className={styles.container}>
-      <h2>Profile information</h2>
       <div className={styles.containerBox}>
         <Animation />
-        <div className={styles.info}>
-          <div className={styles.box1}>
-            <div className={styles.fields}>
-              <h5>Name</h5>
-              <p>{adminAccount.name}</p>
+        <div>
+          <h2>Profile information</h2>
+          <div className={styles.info}>
+            <div className={styles.box1}>
+              <div className={styles.fields}>
+                <h5>Name</h5>
+                <p>{adminAccount.name}</p>
+              </div>
+              <div className={styles.fields}>
+                <h5>Last Name</h5>
+                <p>{adminAccount.lastName}</p>
+              </div>
             </div>
-            <div className={styles.fields}>
-              <h5>Last Name</h5>
-              <p>{adminAccount.lastName}</p>
-            </div>
-          </div>
-          <div className={styles.box2}>
-            <div className={styles.fields}>
-              <h5>Email</h5>
-              <p>{adminAccount.email}</p>
+            <div className={styles.box2}>
+              <div className={styles.fields}>
+                <h5>Email</h5>
+                <p>{adminAccount.email}</p>
+              </div>
             </div>
           </div>
         </div>
