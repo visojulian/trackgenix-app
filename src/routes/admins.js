701df1af71c353eb38a10a94e74c268446d271f1
Maxi Delfino <maxidelfino1234@hotmail.com>
2022-12-21 12:13:23 -0300

    TG-185: Add spinner to tables

--- src/routes/admins.js
+++ src/routes/admins.js
@@ -1,5 +1,6 @@
 import Layout from 'Components/Layout';
-import React, { lazy } from 'react';
+import Spinner from 'Components/Shared/Spinner';
+import React, { lazy, Suspense } from 'react';
 import { useRouteMatch, Redirect, Route, Switch } from 'react-router-dom';
 const AdminProfile = lazy(() => import('Components/Admins/AdminProfile'));
 const Employees = lazy(() => import('Components/Employees'));
@@ -24,22 +25,24 @@ const AdminsRouter = () => {
   const { url } = useRouteMatch();
   return (
     <Layout routes={routes}>
-      <Switch>
-        <Route exact path={`${url}/home`} component={Home} />
-        <Route exact path={`${url}/admin/projects`} component={Projects} />
-        <Route exact path={`${url}/profile`} component={AdminProfile} />
-        <Route exact path={`${url}/employees/form`} component={EmployeeForm} />
-        <Route path={`${url}/employees/form/:id`} component={EmployeeForm} />
-        <Route exact path={`${url}/employees`} component={Employees} />
-        <Route exact path={`${url}/projects`} component={Projects} />
-        <Route exact path={`${url}/projects/form`} component={ProjectsForm} />
-        <Route path={`${url}/projects/form/:id`} component={ProjectsForm} />
-        <Route exact path={`${url}/tasks`} component={Tasks} />
-        <Route exact path={`${url}/tasks/form`} component={TasksForm} />
-        <Route path={`${url}/tasks/form/:id`} component={TasksForm} />
-        <Route exact path={`${url}/timesheets`} component={TimeSheets} />
-        <Redirect to={`${url}/projects`} />
-      </Switch>
+      <Suspense fallback={<Spinner isLoading={true} />}>
+        <Switch>
+          <Route exact path={`${url}/home`} component={Home} />
+          <Route exact path={`${url}/admin/projects`} component={Projects} />
+          <Route exact path={`${url}/profile`} component={AdminProfile} />
+          <Route exact path={`${url}/employees/form`} component={EmployeeForm} />
+          <Route path={`${url}/employees/form/:id`} component={EmployeeForm} />
+          <Route exact path={`${url}/employees`} component={Employees} />
+          <Route exact path={`${url}/projects`} component={Projects} />
+          <Route exact path={`${url}/projects/form`} component={ProjectsForm} />
+          <Route path={`${url}/projects/form/:id`} component={ProjectsForm} />
+          <Route exact path={`${url}/tasks`} component={Tasks} />
+          <Route exact path={`${url}/tasks/form`} component={TasksForm} />
+          <Route path={`${url}/tasks/form/:id`} component={TasksForm} />
+          <Route exact path={`${url}/timesheets`} component={TimeSheets} />
+          <Redirect to={`${url}/projects`} />
+        </Switch>
+      </Suspense>
     </Layout>
   );
 };
--- src/routes/employees.js
+++ src/routes/employees.js
@@ -1,5 +1,6 @@
 import Layout from 'Components/Layout';
-import React, { lazy } from 'react';
+import Spinner from 'Components/Shared/Spinner';
+import React, { lazy, Suspense } from 'react';
 import { useRouteMatch, Redirect, Route, Switch } from 'react-router-dom';
 const Projects = lazy(() => import('Components/Employee/Projects'));
 const ProjectForm = lazy(() => import('Components/Employee/Projects/Form'));
@@ -19,18 +20,20 @@ const EmployeesRouter = () => {
   const { url } = useRouteMatch();
   return (
     <Layout routes={routes}>
-      <Switch>
-        <Route exact path={`${url}/home`} component={Home} />
-        <Route exact path={`${url}/employee/timesheets`} component={Timesheets} />
-        <Route exact path={`${url}/projects`} component={Projects} />
-        <Route exact path={`${url}/profile`} component={EmployeeProfile} />
-        <Route exact path={`${url}/profile/edit`} component={EmployeeProfileEdit} />
-        <Route exact path={`${url}/projects/form/:id`} component={ProjectForm} />
-        <Route exact path={`${url}/timesheets`} component={Timesheets} />
-        <Route exact path={`${url}/timesheets/form`} component={TimeSheetsForm} />
-        <Route path={`${url}/timesheets/form/:id`} component={TimeSheetsForm} />
-        <Redirect to={`${url}/timesheets`} />
-      </Switch>
+      <Suspense fallback={<Spinner isLoading={true} />}>
+        <Switch>
+          <Route exact path={`${url}/home`} component={Home} />
+          <Route exact path={`${url}/employee/timesheets`} component={Timesheets} />
+          <Route exact path={`${url}/projects`} component={Projects} />
+          <Route exact path={`${url}/profile`} component={EmployeeProfile} />
+          <Route exact path={`${url}/profile/edit`} component={EmployeeProfileEdit} />
+          <Route exact path={`${url}/projects/form/:id`} component={ProjectForm} />
+          <Route exact path={`${url}/timesheets`} component={Timesheets} />
+          <Route exact path={`${url}/timesheets/form`} component={TimeSheetsForm} />
+          <Route path={`${url}/timesheets/form/:id`} component={TimeSheetsForm} />
+          <Redirect to={`${url}/timesheets`} />
+        </Switch>
+      </Suspense>
     </Layout>
   );
 };
